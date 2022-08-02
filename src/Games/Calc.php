9a218054c41ed43e91a49727e3523804c0275b3c
VaLeraGav <deliciiouss@yandex.ru>
2022-08-02 13:56:52 +0300

    test

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -3,28 +3,33 @@
 namespace BrainGames\Games\Calc;
 
 use FFI\Exception;
+use function BrainGames\Engine\run;
 
+const DESCRIPTION = "What is the result of the expression?";
 
 function calculator()
 {
-    $first = rand(1, 100);
-    $second = rand(1, 100);
-    $signs = ['-', '+', '*'];
-    $sign = $signs[array_rand($signs)];
-    $question = "{$first}{$sign}{$second}";
-    switch ($sign) {
-        case '-':
-            $result = $first - $second;
-            break;
-        case '+':
-            $result = $first + $second;
-            break;
-        case '*':
-            $result = $first * $second;
-            break;
-        default:
-            throw new Exception('There is no such operator: {$sign}.');
-    }
-    print_r($result);
-    return [$question,$result];
+    $runGame = function () {
+        $first = rand(1, 100);
+        $second = rand(1, 100);
+        $signs = ['-', '+', '*'];
+        $sign = $signs[array_rand($signs)];
+        $question = "{$first}{$sign}{$second}";
+        switch ($sign) {
+            case '-':
+                $result = $first - $second;
+                break;
+            case '+':
+                $result = $first + $second;
+                break;
+            case '*':
+                $result = $first * $second;
+                break;
+            default:
+                throw new Exception('There is no such operator: {$sign}.');
+        }
+        print_r($result);
+        return [$question, $result];
+    };
+    run(DESCRIPTION, $runGame);
 }
