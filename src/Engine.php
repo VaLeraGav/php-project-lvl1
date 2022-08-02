8bfa5921b001c8411eeb54bcc87bda31797fdef3
VaLeraGav <deliciiouss@yandex.ru>
2022-08-02 13:36:40 +0300

    add brain-calc and Engine.php

---
+++ src/Engine.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+// function run(string $description, callable $runGame, int $roundsCount = 3)
+// {
+//     line('Welcome to the Brain Games!');
+//     $name = prompt('May I have your name?');
+//     line("Hello, %s!", $name);
+//     line($description);
+
+//     for ($i = 0; $i < $roundsCount; $i++) {
+//         [$question, $rightAnswer] = $runGame();
+//         line("Question: {$question}");
+//         $answerUser = prompt("You answer");
+
+//         if ($rightAnswer === $answerUser) {
+//             line("Correct!");
+//         } else {
+//             line("'{$answerUser}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
+//             line("Let's try again, {$name}");
+//             return;
+//         }
+//         line("Correct!");
+//     }
+//     line("Congratulations, {$name}!");
+// }
